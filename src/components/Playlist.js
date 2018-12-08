4f94c870ab7961a01d2a176dbad27baa93754921
BarbaraWidjono <barbarawidjono@gmail.com>
2018-12-08 14:19:19 -0800

    Converted Playlist to class

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -4,53 +4,63 @@ import './styles/Playlist.css';
 
 import Track from './Track';
 
-const calculatePlayTime = (tracks) => {
-  let minutes = 0;
-  let seconds = 0;
-  tracks.forEach((track) => {
-    const times = track.playtime.split(':');
-    minutes += parseInt(times[0]);
-    seconds += parseInt(times[1]);
-  });
+class Playlist extends React.Component{
+  constructor(props){
+    super(props);
 
-  minutes += Math.floor(seconds / 60);
-  const hours = Math.floor(minutes / 60);
+    this.state = {
+      tracks: props.tracks,
+    }
+  }
 
-  seconds %= 60;
-  minutes %= 60;
+  calculatePlayTime = (tracks) => {
+    let minutes = 0;
+    let seconds = 0;
+    tracks.forEach((track) => {
+      const times = track.playtime.split(':');
+      minutes += parseInt(times[0]);
+      seconds += parseInt(times[1]);
+    });
 
-  seconds = ("" + seconds).padStart(2, "0");
-  minutes = ("" + minutes).padStart(2, "0");
+    minutes += Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
 
-  return `${hours}:${minutes}:${seconds}`;
-}
+    seconds %= 60;
+    minutes %= 60;
 
-const Playlist = (props) => {
-  const tracks = props.tracks;
-  const trackCount = tracks.length;
-  const playtime = calculatePlayTime(tracks);
-  const trackElements = tracks.map((track, i) => {
-    // We use "spread syntax" here to pass in all the properties of
-    // the variable 'track' as props. Go look it up!
+    seconds = ("" + seconds).padStart(2, "0");
+    minutes = ("" + minutes).padStart(2, "0");
+
+    return `${hours}:${minutes}:${seconds}`;
+  }
+
+  render(){
+
+    const trackCount = this.state.tracks.length;
+    const playtime = this.calculatePlayTime(this.state.tracks);
+    const trackElements = this.state.tracks.map((track, i) => {
+      // We use "spread syntax" here to pass in all the properties of
+      // the variable 'track' as props. Go look it up!
+      return (
+        <Track
+          key={i}
+          {...track}
+        />
+      );
+    });
+
     return (
-      <Track
-        key={i}
-        {...track}
-      />
-    );
-  });
-
-  return (
-    <div className="playlist">
-      <h2>{props.side} Playlist</h2>
-      <p>
-        {trackCount} tracks - {playtime}
-      </p>
-      <ul className="playlist--track-list">
-        {trackElements}
-      </ul>
-    </div>
-  );
+      <div className="playlist">
+        <h2>{this.props.side} Playlist</h2>
+        <p>
+          {trackCount} tracks - {playtime}
+        </p>
+        <ul className="playlist--track-list">
+          {trackElements}
+        </ul>
+      </div>
+    )
+  }
 }
 
 Playlist.propTypes = {
